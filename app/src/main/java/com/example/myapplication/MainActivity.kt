bf3069a027562a2400fd88f3ae95f7243bd76354
sumit <sumit.dev2148@gmail.com>
2024-05-07 12:01:42 +0530

    Chapter 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,7 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,19 +30,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(widthDp = 450, heightDp = 100)
+@Preview(widthDp = 450, heightDp = 100, backgroundColor = 111111)
 @Composable
 fun showView(){
     Text(
         text = "Hello Sumit kumar sharma",
-        color = Color.Green
+        color = Color.Green,
+        fontSize = 36.sp,
+        fontWeight = FontWeight.ExtraBold,
+        fontStyle = FontStyle.Italic
     )
 }
 
 
-@Preview(widthDp = 450, heightDp = 100)
+@Preview(showBackground = true, widthDp = 750, heightDp = 450)
 @Composable
 fun showView2(){
-    Text(text = "Hello Sumit kumar sharma 22",
-        color = Color.Blue)
+    Image(
+        painter = painterResource(id = R.drawable.baseline_castle_24),
+        contentDescription = "callte image",
+        contentScale = ContentScale.Crop,
+    )
 }
